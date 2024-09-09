a993eeaad88fa47fa5a2471ba2054de7c091895e
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-09 09:42:40 -0300

    calcule o imposto de um produto

---
+++ atividade07.py
@@ -0,0 +1,6 @@
+# Enunciado: Uma loja aplica um imposto de 12% sobre o valor dos produtos. Crie um programa que receba o preço de um produto e calcule o preço final com o imposto incluído.
+p = float(input(" preço "))
+
+v2 = p*0.12+p
+print(f"preço com imposto {v2}")
+
